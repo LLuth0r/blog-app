5b845360184fa28044ce9146bee6c35f56fe445b
Hunter <huntermott42@gmail.com>
2020-12-18 12:41:24 -0700

    basic skeleton

---
+++ server.js
@@ -0,0 +1,17 @@
+const logger = require('morgan')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const postsRoutes = require('./routes/posts')
+const PORT = 3000 || process.env.PORT
+const db = require('./db/connection')
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(logger('dev'))
+app.use('/api', postsRoutes)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
